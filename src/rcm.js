c3198cfbc86c13dcd40dfbc259d59645196c0deb
Eldar Djafarov <djkojb@gmail.com>
2013-05-14 06:07:11 -0300

    updates

--- src/rcm.js
+++ src/rcm.js
@@ -7,6 +7,10 @@ var _ = require('underscore');
 var log = console.log;
 var filed = require('filed');
 var async = require('async');
+var log = require('winston');
+
+var depTree = {};
+var paths = {};
 
 var commands = {
   init: function(args, callback){
@@ -20,23 +24,60 @@ var commands = {
     if(!comps) return console.log('no components to install');
     
     async.eachSeries(_(comps).keys(), function(compname, cb){
-      console.log("#######START " + compname);
-      var pk = component.install( compname, comps[compname], {remotes: ['https://raw.github.com']});
-      report(pk);
-      prepare(pk);
-      pk.on('end',function(){
-        console.log("!!!!!END " + pk.name);
-        cb();
-      })
-      pk.install();
+      installPkg(compname, comps[compname],cb);
+    }, function(err){
+      fs.writeFile(process.cwd() + '/require.cfg.json', JSON.stringify({paths:paths, shim:depTree}), done);
+      function done(){
+        log.info("require.cfg.json done!");
+      }
     });
-    
     //TODO: without args install everything from package.json
     //TODO: with args install componetns and modify to work properly
   }
 }
 
+function installPkg(pkgName, ver, cb){
+  log.info("START - install " + pkgName);
+  if(component.exists(pkgName)) return cb();
+  var pk = component.install( pkgName, ver, {remotes: ['https://raw.github.com']});
+  report(pk);
+  buildDepTree(pk);
+  buildPaths(pk);
+  pk.on('end',function(){
+    log.info("END - install " + pkgName);
+    cb();
+  })
+  pk.install();
+}
 
+function buildDepTree(pkg){
+  pkg.on('dep', function(dep){
+    buildDepTree(dep);
+  });
+  pkg.on('end', function(){
+    var dirName = pkg.name.replace("/","-");
+    var pkgName = dirName.split("-")[1];
+    var compconf = require(process.cwd() + '/components/' + dirName + '/component.json');
+    var depsArr = compconf.dependencies && _(compconf.dependencies).chain().keys().map(function(el){
+      return el.split("/")[1];
+    }).value();
+    if(!_(depsArr).isEmpty()) return depTree[pkgName] = {deps:depsArr,exports:'module.exports'};
+    depTree[pkgName] = {exports:'module.exports'};
+  });
+}
+
+function buildPaths(pkg){
+  pkg.on('dep', function(dep){
+    buildPaths(dep);
+  });
+  pkg.on('end', function(){
+    var dirName = pkg.name.replace("/","-");
+    var pkgName = dirName.split("-")[1];
+    var compconf = require(process.cwd() + '/components/' + dirName + '/component.json');
+    paths[pkgName] = '/components/' + dirName + "/" + compconf.scripts[0].replace(".js","");
+    paths[pkg.name] = '/components/' + dirName + "/" + compconf.scripts[0].replace(".js","");
+  });
+}
 
 commander
   .version('0.0.5')
@@ -47,68 +88,33 @@ commander
 //commands.init("someapp");
 commands.install();
 
-
-function prepare(pkg){
-  pkg.on("end", function(){
-    var dirName = pkg.name.replace("/","-");
-    var compconf = require(process.cwd() + '/components/' + dirName + '/component.json');
-    var depsArr = compconf.dependencies && _(compconf.dependencies).chain().keys().map(function(el){
-      return el.replace("/","-");
-    }).value();
-    
-    _(compconf.scripts).each(boost);
-    var index = fs.createWriteStream(process.cwd() + '/components/' + dirName + '/rcm_index.js');
-    var scripts = _(compconf.scripts).map(function(script){
-      return "./components/" + dirName + "/" + script;
-    });
-    index.write("define(['"+ scripts.join("','")+"'], function(ret){return ret});\n");
-    index.end();
-
-    function boost(script){
-      fs.readFile(process.cwd() + '/components/' + dirName +"/"+ script, gotIt);
-      function gotIt(err, data){
-        var reqDeps = _(depsArr).map(function(dep){return dep.split("-")[1]});
-        var deps = (reqDeps && reqDeps.length > 0) ? (",'" + reqDeps.join("','") + "'"):"";
-        var newData = "define(['require','module','exports'"+deps+"], function(require, module){\n";
-        newData+= data;
-        newData+= "\n});";
-        fs.writeFile(process.cwd() + '/components/' + dirName +"/"+ script, newData, done);
-      }
-      function done(){
-        console.log("DONE: "+dirName);
-      }
-    }
-  })
-}
-
 function report(pkg, options) {
   options = options || {};
   if (pkg.inFlight) return;
-  log('install', pkg.name + '@' + pkg.version);
+  log.info('install', pkg.name + '@' + pkg.version);
 
   pkg.on('error', function(err){
     if (err.fatal) {
-      log(err.message);
+      log.info(err.message);
       process.exit(1);
     }
   });
 
   pkg.on('dep', function(dep){
-    log('dep', dep.name + '@' + dep.version);
+    log.info('dep', dep.name + '@' + dep.version);
     report(dep, options);
-    prepare(dep);
   });
 
   pkg.on('exists', function(dep){
-    log('exists', dep.name + '@' + dep.version);
+    log.info('exists', dep.name + '@' + dep.version);
   });
 
   pkg.on('file', function(file){
-    log('fetch', pkg.name + ':' + file);
+    log.info('fetch', pkg.name + ':' + file);
   });
 
   pkg.on('end', function(){
-    log('complete', pkg.name);
+    log.info('complete', pkg.name);
   });
 }
 
